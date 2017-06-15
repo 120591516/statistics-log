07b28e5592d6fde1b489f2a985e615b0593b4f09
120591516 <120591516@qq.com>
2017-06-15 21:53:58 +0800

    a

--- src/main/java/com/jinpaihushi/StatisticsLogApplication.java
+++ src/main/java/com/jinpaihushi/StatisticsLogApplication.java
@@ -2,11 +2,13 @@ package com.jinpaihushi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class StatisticsLogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StatisticsLogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsLogApplication.class, args);
+    }
 }
