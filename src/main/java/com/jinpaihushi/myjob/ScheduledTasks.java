5e3eb674c126739aed3b34b1adf758b6a70f8430
wangwenteng <wangwenteng@llvision.com>
2018-03-06 17:27:33 +0800

    asd

--- src/main/java/com/jinpaihushi/myjob/ScheduledTasks.java
+++ src/main/java/com/jinpaihushi/myjob/ScheduledTasks.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -28,9 +29,11 @@ public class ScheduledTasks {
 
     @Autowired
     private ParseYykLog parseYykLog;
-
+	@Value("${SMS_Verification_Code}")
+	private String SMS_Verification_Code;
     @Scheduled(cron = "${cron}")
     public void reportCurrentTime() {
+		System.out.println("加载到注解 SMS_Verification_Code=" + SMS_Verification_Code);
         System.out.println("Scheduling Tasks Examples: The time is now " + dateFormat().format(new Date()));
         logger.info("开始解析微信公众号日志");
         parseWxLog.readFileByLines();
